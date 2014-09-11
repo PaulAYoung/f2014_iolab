2209f3f77596038a674764c51950b70eb2590882
Paul Andres Young <paulandresyoung@gmail.com>
2014-09-10 22:38:20 -0700

    homework1

---
+++ homework-1/README.md
@@ -0,0 +1,6 @@
+#Homework 1
+Paul Young
+
+I decided to go with stop motion shorts because it is a pretty cool artform that doesn't get much love these days. 
+
+You can view the collection stating with index.html. 
--- hw1_datascraper.py
+++ hw1_datascraper.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import urllib
+
 from bs4 import BeautifulSoup
 import requests
 from jinja2 import Template
@@ -22,6 +24,7 @@ def get_data(name):
     key_vals["url"] = url
     key_vals["embed"] = embed
     key_vals["name"] = name
+    key_vals["link"] = urllib.quote(name.replace(" ", "_"))
 
     key_vals["producer"] = key_vals["Producer:"]
     del key_vals["Producer:"]
@@ -32,26 +35,66 @@ def get_data(name):
 
     return key_vals
 
+test_data = [
+        {
+            "name": "Muto",
+            "link": "muto",
+            "producer": "Blu",
+            "embed": """<iframe src="https://archive.org/embed/Muto-animation" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>""",
+            "keywords": "animation; blu; stop-motion"
+            },
+        {
+            "name": "Gumbasia",
+            "link": "gumbasion",
+            "producer": "Art Clokey",
+            "embed": """<iframe src="https://archive.org/embed/Gumbasia_331" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>""",
+            "keywords": "Art Clokey; Gumby; Stop-motion; Claymation; television"
+            },
+        {
+            "name": "The Ermo Show",
+            "link": "theermoshow",
+            "producer": "Delaware Based Productions",
+            "embed": """<iframe src="https://archive.org/embed/the_ermo_show" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>""",
+            "keywords": "Ermo; Puppet; Animation; Stop-Motion; Funny; Cartoon; Beans; Hats; Nathan Smithe (silent e)"
+            },
+        {
+            "name": "Malvin and the Chipdudes!",
+            "link": "malvin",
+            "producer": "Delaware Based Productions",
+            "embed": """<iframe src="https://archive.org/embed/malvin_and_the_chipdudes" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>""",
+            "keywords": "Animation; Humor; Stop motion; Clay; Compositing; Cover; Sped up; Speed; Tweak; Bitch"
+            },
+        {
+            "name": "The Mascot - Complete and Uncut",
+            "link": "the_mascot",
+            "producer": "Ladislas Starewicz",
+            "embed": """<iframe src="https://archive.org/embed/The_Mascot_Complete" width="640" height="480" frameborder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen></iframe>""",
+            "keywords": "stop motion, animation, Wladyslaw, Starevich, Starevitch, Starewich, Starewitch, short film, Fetiche, Mascotte, Devil's Ball"
+            }
+  ]
 
 def main():
     shorts = [
-        "Muto-animation"
+        "Muto-animation",
+        "Gumbasia",
+        "The Ermo Show",
+        "Malvin and the Chipdudes!",
+        "The Mascot - Complete and Uncut"
         ]
-    data = []
+    data = test_data
 
     #load templates
     index_template = Template(open("hwhelpers/short_list.html", "r").read())
     item_template = Template(open("hwhelpers/short_item.html", "r").read())
 
-    for s in shorts:
-        print("Creating file for {}".format(s))
-        data.append(get_data(s))
-        out_file = open("./homework-1/{}.html".format(s), "w")
-        out_file.write(item_template.render(data[-1]))
+    for d in data:
+        print "writing page for {}".format(d["name"])
+        out_file = open("./homework-1/{}.html".format(d["link"]), "w")
+        out_file.write(item_template.render(d))
         out_file.close()
 
     print("Writing index")
-    index = open("./homework-1/index.html")
+    index = open("./homework-1/index.html", "w")
     index.write(index_template.render(shorts=data))
     index.close()
 
