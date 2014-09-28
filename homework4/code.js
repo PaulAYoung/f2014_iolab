d4f9de59a6e9f323699cb6bf5ba8a307f1c0ff9d
Paul Andres Young <paulandresyoung@gmail.com>
2014-09-27 21:00:57 -0700

    hw4

--- homework4/code.js
+++ homework4/code.js
@@ -1,31 +1,29 @@
-
-function clear_table(){
-    data_table("tr").remove();
-}
-
-function add_data_item(feature){
+function add_data_item(feature, data_table){
     console.log(feature);
-    var data_table = $("#earthquake-table tr:last");
     var row = 
         "<tr>" +
-            "<ti>" + feature.properties.time + "</ti>" + //time
-            "<ti>" + feature.properties.time + "</ti>" + //long
-            "<ti>" + feature.properties.time + "</ti>" + //lat
-            "<ti>" + feature.properties.time + "</ti>" + //depth
+            "<td>" + feature.properties.mag + "</td>" + //time
+            "<td>" + feature.properties.place + "</td>" + //long
+            "<td>" + new Date(feature.properties.time) + "</td>" + //lat
+            "<td>" + new Date(feature.properties.updated) + "</td>" + //depth
         "</tr>"; //end row element
     console.log(row);
     data_table.append(row);
-
 }
 
-function get_data(){
+function get_data(table_id){
     $.ajax("http://io.milowski.com/usgs/earthquakes/feed/v1.0/summary/all_hour.geojson",
         {
             success: function(d){
                 var features = d.features;
+                var data_table = $(table_id);
 
+                // clear table
+                data_table.children().remove();
+
+                // Add new content
                 for (var idx=0; idx< features.length; idx ++){
-                    add_data_item(features[idx]);
+                    add_data_item(features[idx], data_table);
                 }
             }
         }
