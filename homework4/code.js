0c483a883dfc8e752a6c344b44fb7d6f0189b428
Paul Young <paulandresyoung@gmail.com>
2014-09-30 12:40:55 -0700

    make it more object oriented

--- homework4/code.js
+++ homework4/code.js
@@ -1,17 +1,27 @@
-function add_data_item(feature, data_table){
+function EarthquakeSpotter(domid, data_url){
+    if (typeof(data_url)==='undefined'){data_url="http://io.milowski.com/usgs/earthquakes/feed/v1.0/summary/all_hour.geojson"}
+
+    this.data_url = data_url;
+    this.root = $(domid);
+    this.data_table = this.root.find(".earthquake-table > tbody:last");
+
+    this.root.find(".earthquake-refresh-button").click($.proxy(this.get_data, this));
+}
+
+EarthquakeSpotter.prototype.add_data_item = function(feature){
     console.log(feature);
     var row = 
         "<tr>" +
             "<td>" + feature.properties.mag + "</td>" + //time
             "<td>" + feature.properties.place + "</td>" + //long
-            "<td>" + format_date(feature.properties.time) + "</td>" + //lat
-            "<td>" + format_date(feature.properties.updated) + "</td>" + //depth
+            "<td>" + this.format_date(feature.properties.time) + "</td>" + //lat
+            "<td>" + this.format_date(feature.properties.updated) + "</td>" + //depth
         "</tr>"; //end row element
     console.log(row);
-    data_table.append(row);
+    this.data_table.append(row);
 }
 
-function format_date(date){
+EarthquakeSpotter.prototype.format_date =  function(date){
     //Take a date and return string in "mm/dd/yyyy hh:mm" format
     date = new Date(date);
     var m = date.getMonth() + 1;
@@ -26,19 +36,20 @@ function format_date(date){
     return m + "/" + d + "/" + y + " " + t;
 }
 
-function get_data(table_id){
-    $.ajax("http://io.milowski.com/usgs/earthquakes/feed/v1.0/summary/all_hour.geojson",
+EarthquakeSpotter.prototype.get_data =  function(){
+    data_table = this.data_table;
+    $.ajax(this.data_url,
         {
+            context: this,
             success: function(d){
                 var features = d.features;
-                var data_table = $(table_id);
 
                 // clear table
-                data_table.children().remove();
+                this.data_table.children().remove();
 
                 // Add new content
                 for (var idx=0; idx< features.length; idx ++){
-                    add_data_item(features[idx], data_table);
+                    this.add_data_item(features[idx], data_table);
                 }
             }
         }
