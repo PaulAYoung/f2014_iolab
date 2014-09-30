c0ecd5053cdfe409e164b0880ead260d36197418
Paul Young <paulandresyoung@gmail.com>
2014-09-30 12:08:58 -0700

    date formatting

--- homework4/code.js
+++ homework4/code.js
@@ -4,11 +4,26 @@ function add_data_item(feature, data_table){
         "<tr>" +
             "<td>" + feature.properties.mag + "</td>" + //time
             "<td>" + feature.properties.place + "</td>" + //long
-            "<td>" + new Date(feature.properties.time) + "</td>" + //lat
-            "<td>" + new Date(feature.properties.updated) + "</td>" + //depth
+            "<td>" + format_date(feature.properties.time) + "</td>" + //lat
+            "<td>" + format_date(feature.properties.updated) + "</td>" + //depth
         "</tr>"; //end row element
     console.log(row);
     data_table.append(row);
+}
+
+function format_date(date){
+    //Take a date and return string in "mm/dd/yyyy hh:mm" format
+    date = new Date(date);
+    var m = date.getMonth() + 1;
+    var y = date.getFullYear();
+    var d = date.getDate();
+    var t = date.getHours() + ":";
+    if (date.getMinutes() > 9){
+        t = t + date.getMinutes();
+    } else {
+        t = t + "0" +  date.getMinutes();
+    }
+    return m + "/" + d + "/" + y + " " + t;
 }
 
 function get_data(table_id){
